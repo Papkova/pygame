20c73ba94d2d0a110af153f98c296a19ad0171ec
Diana <papkavadiana@gmail.com>
2024-11-10 22:08:15 +0200

    Привиди в ігрі

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ icon = pygame.image.load('images/icon.png.png')
 pygame.display.set_icon(icon)
 
 
-bg = pygame.image.load("images/bg.png.png")
+bg = pygame.image.load("images/bg.png.png").convert()
 walk_left = [
-    pygame.image.load("images/player_left/player_left1.png"),
-    pygame.image.load("images/player_left/player_left2.png"),
-    pygame.image.load("images/player_left/player_left3.png"),
-    pygame.image.load("images/player_left/player_left4.png")
+    pygame.image.load("images/player_left/player_left1.png").convert_alpha(),
+    pygame.image.load("images/player_left/player_left2.png").convert_alpha(),
+    pygame.image.load("images/player_left/player_left3.png").convert_alpha(),
+    pygame.image.load("images/player_left/player_left4.png").convert_alpha()
+]
+walk_right = [
+    pygame.image.load("images/player_right/player_right1.png").convert_alpha(),
+    pygame.image.load("images/player_right/player_right2.png").convert_alpha(),
+    pygame.image.load("images/player_right/player_right3.png").convert_alpha(),
+    pygame.image.load("images/player_right/player_right4.png").convert_alpha()
 ]
 
-walk_right = [
-    pygame.image.load("images/player_right/player_right1.png"),
-    pygame.image.load("images/player_right/player_right2.png"),
-    pygame.image.load("images/player_right/player_right3.png"),
-    pygame.image.load("images/player_right/player_right4.png")
-]
+ghost = pygame.image.load("images/ghost.png").convert_alpha()
+ghost_list_in_game = []
 
 player_anim_count = 0
 bg_x = 0
@@ -32,18 +34,30 @@ player_x = 150
 player_y = 250
 
 is_jump = False
-jump_count = 7
+jump_count = 8
 
 
 bg_sounds = pygame.mixer.Sound("sounds/creepy.mp3.mp3")
 bg_sounds.play()
 
+ghost_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(ghost_timer, 2500)
 
 running = True
 while running:
 
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 618, 0))
+
+    player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))
+
+    if ghost_list_in_game:
+        for el in ghost_list_in_game:
+            screen.blit(ghost, el)
+            el.x -= 10
+
+            if player_rect.colliderect(el):
+                print("You lose")
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
@@ -60,7 +74,7 @@ while running:
         if keys[pygame.K_SPACE]:
             is_jump = True
     else:
-        if jump_count >= -7:
+        if jump_count >= -8:
             if jump_count > 0:
                 player_y -= (jump_count ** 2) / 2
             else:
@@ -85,5 +99,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+        if event.type == ghost_timer:
+            ghost_list_in_game.append(ghost.get_rect(topleft=(620, 250)))
 
     clock.tick(15)
