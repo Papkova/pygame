ec91b5e24b3692f63e517cbdbfd1eed5aa8ea705
Diana <papkavadiana@gmail.com>
2024-11-10 21:21:28 +0200

    рух і прижки

--- main.py
+++ main.py
@@ -27,6 +27,14 @@ walk_right = [
 player_anim_count = 0
 bg_x = 0
 
+player_speed = 5
+player_x = 150
+player_y = 250
+
+is_jump = False
+jump_count = 7
+
+
 bg_sounds = pygame.mixer.Sound("sounds/creepy.mp3.mp3")
 bg_sounds.play()
 
@@ -36,7 +44,31 @@ while running:
 
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 618, 0))
-    screen.blit(walk_right[player_anim_count], (300, 250))
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        screen.blit(walk_left[player_anim_count], (player_x, player_y))
+    else:
+        screen.blit(walk_right[player_anim_count], (player_x, player_y))
+
+    if keys[pygame.K_LEFT] and player_x > 50:
+        player_x -= player_speed
+    elif keys[pygame.K_RIGHT] and player_x < 200:
+        player_x += player_speed
+
+    if not is_jump:
+        if keys[pygame.K_SPACE]:
+            is_jump = True
+    else:
+        if jump_count >= -7:
+            if jump_count > 0:
+                player_y -= (jump_count ** 2) / 2
+            else:
+                player_y += (jump_count ** 2) / 2
+            jump_count -= 1
+        else:
+             is_jump = False
+             jump_count = 7
 
     if player_anim_count == 3:
         player_anim_count = 0
